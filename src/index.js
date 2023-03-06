d0949eee990491f4f208e9dc0c162cb076513fd7
OleksandraDana <110694602+OleksandraDana@users.noreply.github.com>
2023-03-06 00:11:13 +0000

    Created the time of the weather last updates
    Function formatDate

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-let now = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[now.getDay()];
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let updatedDate = document.querySelector("#current-time");
-updatedDate.innerHTML = `${day}, ${hours}:${minutes}`;
 
 //challenge2
 
@@ -55,6 +55,9 @@ function showTemperature(response) {
 
   document.querySelector("#description").innerHTML =
     response.data.weather[0].description;
+  document.querySelector("#current-time").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
 }
 // position challenge
 function getCurrentLocation() {
